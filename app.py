ba5ba9e62edf5fc0aaaa440a63c4210324751257
RB-01 <rohitbhatia964@gmail.com>
2021-09-29 21:17:30 +0530

    student id made dynamic in sql command in student_page function

--- app.py
+++ app.py
@@ -145,8 +145,9 @@ def student_page():
     #     name = session['std_name']
         print(current_user)
         print(current_user.id)
+        student_id = current_user.id
         # intervw_list = Interviewer.query.all()
-        result = db.session.execute('select i.username u,i.company_name cn,j.job_profile jp from interviewer i join jobs j on j.interviewer_id = i.id join job_stu_map js on js.job_id=j.job_id where js.stu_id=2')
+        result = db.session.execute(f'select i.username u,i.company_name cn,j.job_profile jp from interviewer i join jobs j on j.interviewer_id = i.id join job_stu_map js on js.job_id=j.job_id where js.stu_id={ student_id }')
         return render_template('student/dashboard.html',name = current_user.username, jobs_list = result)
     # else:    
     #     return redirect(url_for("student_login"))
